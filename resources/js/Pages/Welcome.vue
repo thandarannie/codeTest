34a8de236335ef8022f8fa9ed100bd150d8cad67
thandarannie <58065319+thandarannie@users.noreply.github.com>
2023-09-05 20:48:42 +0630

    fix

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -30,12 +30,12 @@ const props = defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="grid grid-cols-3 gap-4">
-                    <div class="min-w-sm rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800"
+                    <div class="min-w-sm rounded-lg shadow-xs overflow-hidden bg-white "
                         >
                         <a href="#">
                         <div class="p-4 flex items-center">
                             <div
-                            class="p-3 rounded-full text-green-500 dark:text-green-100 bg-green-100 dark:bg-green-500 mr-4"
+                            class="p-3 rounded-full text-green-500 bg-green-100 mr-4"
                             >
                             <svg fill="currentColor" viewBox="0 0 20 20" class="w-5 h-5">
                                 <path
@@ -44,10 +44,10 @@ const props = defineProps({
                             </svg>
                             </div>
                             <div>
-                            <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                            <p class="mb-2 text-sm font-medium text-gray-600 ">
                                 Total Accounts
                             </p>
-                            <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                            <p class="text-lg font-semibold text-gray-700 ">
                             {{users}}
                             </p>
                             </div>
@@ -56,12 +56,12 @@ const props = defineProps({
                     </div>
 
 
-                    <div class="min-w-sm rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800"
+                    <div class="min-w-sm rounded-lg shadow-xs overflow-hidden bg-white"
                         >
                         <a href="#">
                         <div class="p-4 flex items-center">
                             <div
-                            class="p-3 rounded-full text-orange-500 dark:text-orange-100 bg-orange-100 dark:bg-orange-500 mr-4"
+                            class="p-3 rounded-full text-orange-500  bg-orange-100  mr-4"
                             >
                             <svg fill="currentColor" viewBox="0 0 20 20" class="w-5 h-5">
                                 <path
@@ -70,10 +70,10 @@ const props = defineProps({
                             </svg>
                             </div>
                             <div>
-                            <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                            <p class="mb-2 text-sm font-medium text-gray-600 ">
                                 Total Patients
                             </p>
-                            <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                            <p class="text-lg font-semibold text-gray-700 ">
                             {{patients}}
                             </p>
                             </div>
@@ -81,12 +81,12 @@ const props = defineProps({
                         </a>
                     </div>
 
-                    <div class="min-w-sm rounded-lg shadow-xs overflow-hidden bg-white dark:bg-gray-800"
+                    <div class="min-w-sm rounded-lg shadow-xs overflow-hidden bg-white "
                         >
                         <a href="#">
                         <div class="p-4 flex items-center">
                             <div
-                            class="p-3 rounded-full text-blue-500 dark:text-blue-100 bg-blue-100 dark:bg-blue-500 mr-4"
+                            class="p-3 rounded-full text-blue-500  bg-blue-100 "
                             >
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z" />
@@ -94,10 +94,10 @@ const props = defineProps({
 
                             </div>
                             <div>
-                            <p class="mb-2 text-sm font-medium text-gray-600 dark:text-gray-400">
+                            <p class="mb-2 text-sm font-medium text-gray-600 ">
                                Townships
                             </p>
-                            <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                            <p class="text-lg font-semibold text-gray-700 ">
                             {{townships}}
                             </p>
                             </div>
